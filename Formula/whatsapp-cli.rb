7a815f8257526b7bd09e655cc511e77233e13524
Vicente Reig Rincon de Arellano <vicente.reig@gmail.com>
2025-12-13 13:29:06 +0100

    Fix SHA256 hashes for v1.3.2

--- Formula/whatsapp-cli.rb
+++ Formula/whatsapp-cli.rb
@@ -7,22 +7,22 @@ class WhatsappCli < Formula
   on_macos do
     on_intel do
       url "https://github.com/vicentereig/whatsapp-cli/releases/download/v#{version}/whatsapp-cli-darwin-amd64.tar.gz"
-      sha256 "Not"
+      sha256 "04647174bccf38af4de4d3b0d2edef33ae2e8500a4e5f5088e55e5998f7be021"
     end
     on_arm do
       url "https://github.com/vicentereig/whatsapp-cli/releases/download/v#{version}/whatsapp-cli-darwin-arm64.tar.gz"
-      sha256 "Not"
+      sha256 "619c53dba4e66d41adbab1d537d509fc669ccb699326d8c9651eb2f92dc4120e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/vicentereig/whatsapp-cli/releases/download/v#{version}/whatsapp-cli-linux-amd64.tar.gz"
-      sha256 "Not"
+      sha256 "b8e36e3e8410a47f9eeb7c7d402c8237e8f9b475deea331d0ca443edaa9c0644"
     end
     on_arm do
       url "https://github.com/vicentereig/whatsapp-cli/releases/download/v#{version}/whatsapp-cli-linux-arm64.tar.gz"
-      sha256 "Not"
+      sha256 "f2ed11f502560949a63405be453ac394111e3420ebe0eb0a7869e610ad98e24a"
     end
   end
 
