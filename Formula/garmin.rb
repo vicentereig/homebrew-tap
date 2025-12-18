8be487c477d35035fb51c065d878287bcb80fa5a
Vicente Reig Rincon de Arellano <vicente.reig@gmail.com>
2025-12-18 13:06:47 +0100

    Update garmin placeholder to v1.0.1

--- Formula/garmin.rb
+++ Formula/garmin.rb
@@ -1,7 +1,7 @@
 class Garmin < Formula
   desc "CLI for Garmin Connect API - activities, health metrics, and more"
   homepage "https://github.com/vicentereig/garmin-cli"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
